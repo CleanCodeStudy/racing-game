ded005980425d03bfcd98af8b1af040ea0758509
Malibin <nightmare73@naver.com>
2020-03-19 21:51:14 +0900

    [add] car class 작성

---
+++ src/main/java/racing/Car.java
@@ -0,0 +1,17 @@
+package racing;
+
+public class Car {
+
+    private int drivenDistance = 0;
+
+    private Mover mover = new Mover();
+
+    public void move() {
+        drivenDistance += mover.moveResult();
+    }
+
+    public int getDrivenDistance() {
+        return drivenDistance;
+    }
+
+}
