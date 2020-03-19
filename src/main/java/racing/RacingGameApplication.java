01714284c7c4595914e23769abe9aeec6ff73df3
Malibin <nightmare73@naver.com>
2020-03-19 21:27:32 +0900

    [add] inputHelper 작성

---
+++ src/main/java/racing/RacingGameApplication.java
@@ -0,0 +1,13 @@
+package racing;
+
+public class RacingGameApplication {
+
+    public static void main(String[] args) {
+
+        InputHelper inputHelper = new InputHelper();
+
+        int carNumbers = inputHelper.getCarNumbers();
+        int totalGameTurns = inputHelper.getTotalGameTurns();
+        
+    }
+}
---
+++ src/main/java/racing/view/OutputView.java
@@ -0,0 +1,5 @@
+package racing.view;
+
+public class OutputView {
+
+}
